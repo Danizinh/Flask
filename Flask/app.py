b8cd5069b21d3a57777232729d4ea46193a3777c
Danizinh <danimaltoc@gmai.com>
2022-06-07 23:18:24 -0300

    structuring the project

---
+++ Flask/app.py
@@ -0,0 +1,69 @@
+import secrets
+
+from flask import Flask, flash, redirect, render_template, request, session
+
+
+class Jogo:
+    def __init__(self, name, category, console):
+        self.name = name
+        self.category = category
+        self.console = console
+
+
+# lista global
+Jogo_One = Jogo('Tetris', 'Puzzle', 'Atari')
+Jogo_Two = Jogo('God of  War', 'Rack n Slash', 'PS2')  # referencia do object
+Jogo_three = Jogo('Mortal Kombat', 'Luta', 'PS2')
+lista = [Jogo_One, Jogo_Two, Jogo_three]  # lista reutilizada
+
+app = Flask(__name__)  # referencia ao proprio arquivo
+app.secret_key = '23/04/2022'
+
+
+@app.route('/')  # new rotas
+def index():  # definido os dados de visualizacao
+    # lista local
+    # Referenciando a lista
+    # Jogo_One = Jogo('Tetris', 'Puzzle', 'Atari')
+    # Jogo_Two = Jogo('God of  War', 'Rack n Slash', 'PS2')
+    # Jogo_three = Jogo('Mortal Kombat', 'Luta', 'PS2')
+    # lista = [Jogo_One, Jogo_Two, Jogo_three]
+    return render_template('lista.html', title='Our Games', jogos=lista)
+    # return '<h1>Hello, good morning !! </h1>'
+    # return 'Hello, good morning !!'
+
+
+@app.route('/new')
+def new():
+    return render_template('new.html', title='New Games')
+
+
+@app.route('/create', methods=['POST', ])  # rota de processamento do server
+def create():
+    # obtendo as informacoes do form
+    name = request.form['name']
+    category = request.form['category']
+    console = request.form['console']
+    jogo = Jogo(name, category, console)  # object
+    lista.append(jogo)  # referenciando a propria lista
+    return redirect('/')
+    # return render_template("lista.html", title='Our Games', jogos=lista)
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title="Run your login")
+
+
+@app.route('/autenticar', methods=['POST', ])
+def autenticar():
+    if "alohomora" == request.form['password']:
+        session['logged in user'] = request.form['user']
+        flash(session['logged in user'] + ' user logged in successfully !')
+        return redirect('/')
+    else:
+        flash('user not logged in.')
+        return redirect('/login')
+
+
+app.run(debug=True)
